4b54ac297468c21931e39a33d4fb1f52daffd079
SpongeRobert <painmf@ya.ru>
2018-08-27 02:14:08 +0300

    copy address button added

--- DApp/js/buttons.js
+++ DApp/js/buttons.js
@@ -22,6 +22,39 @@ $(function(){
 });
 
 $(function(){
+    $("#copy_button1").click(function(event){
+        event.preventDefault();
+	navigator.clipboard.writeText(contract1_address).then(function() {
+	    console.log("Copied to clipboard successfully!");
+	}, function() {
+	    console.error("Unable to write to clipboard. :-(");
+	});
+ });
+});
+
+$(function(){
+    $("#copy_button3").click(function(event){
+        event.preventDefault();
+	navigator.clipboard.writeText(contract3_address).then(function() {
+	    console.log("Copied to clipboard successfully!");
+	}, function() {
+	    console.error("Unable to write to clipboard. :-(");
+	});
+ });
+});
+
+$(function(){
+    $("#copy_button4").click(function(event){
+        event.preventDefault();
+	navigator.clipboard.writeText(contract4_address).then(function() {
+	    console.log("Copied to clipboard successfully!");
+	}, function() {
+	    console.error("Unable to write to clipboard. :-(");
+	});
+ });
+});
+
+$(function(){
     $("#debug_gameover").click(function(event){
         event.preventDefault();
         game_over();
